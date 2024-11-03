33959c0f61b1286f6b88fbb0377ffff7c76052b9
harry <gudtjs9556@naver.com>
2024-11-03 18:53:50 +0900

    프로그래머스 Lv2 점프와 순간 이동 solution1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        NLCM nlcm = new NLCM();
+       JumpingAndTeleportation jumpingAndTeleportation = new JumpingAndTeleportation();
 
-        System.out.println(nlcm.solution1(new int[] {2,6,8,14}));
-        System.out.println(nlcm.solution1(new int[] {1,2,3}));
+        System.out.println(jumpingAndTeleportation.solution1(5));
+        System.out.println(jumpingAndTeleportation.solution1(6));
+        System.out.println(jumpingAndTeleportation.solution1(5000));
 
 
     }
