1a22d984417cf615e7d7906b1f74e6bab03fcf6f
harry <gudtjs9556@naver.com>
2024-11-04 17:30:15 +0900

    프로그래머스 Lv2 - 영어 끝말잇기 solution1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,15 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-       JumpingAndTeleportation jumpingAndTeleportation = new JumpingAndTeleportation();
+       ConcludingEnglishEndings concludingEnglishEndings = new ConcludingEnglishEndings();
 
-        System.out.println("solution1 : " + jumpingAndTeleportation.solution1(5));
-        System.out.println("solution1 : " + jumpingAndTeleportation.solution1(6));
-        System.out.println("solution1 : " + jumpingAndTeleportation.solution1(5000));
-        System.out.println("solution2 : " + jumpingAndTeleportation.solution2(5));
-        System.out.println("solution2 : " + jumpingAndTeleportation.solution2(6));
-        System.out.println("solution2 : " + jumpingAndTeleportation.solution2(5000));
+       String[] words = new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+
+        System.out.println(Arrays.toString(concludingEnglishEndings.solution1(3, words)));
 
 
     }
